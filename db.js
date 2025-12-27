b8b22ed54613a3580859336ab22f2b4a5b6ab3d5
Naama Indik <inaama8888@gmail.com>
2025-12-27 17:38:06 +0200

    build react apfpddj

--- db.js
+++ db.js
@@ -21,20 +21,17 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,        // 👈 משתמש
-  password: process.env.MYSQL_PASSWORD, // 👈 סיסמה
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.connect(err => {
-  if (err) {
-    console.error("❌ MySQL ERROR:", err);
-  } else {
-    console.log("✅ MySQL Connected!");
-  }
-});
+console.log("✅ MySQL Pool ready");
 
-module.exports = db;
+module.exports = pool;
