37f985c11bfb6ed107ba08bc5a25002c2d059050
Naama Indik <inaama8888@gmail.com>
2025-12-27 19:09:22 +0200

    build react apfpddeerfffffjfח

--- db.js
+++ db.js
@@ -20,7 +20,14 @@
 require("dotenv").config();
 const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool(process.env.DATABASE_URL);
+const pool = mysql.createPool({
+  uri: process.env.DATABASE_URL,
+
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0,
+  connectTimeout: 10000,
+});
 
 module.exports = pool;
 
