915e1790f9cb8fa33dead4821696f24b44ba9320
Naama Indik <inaama8888@gmail.com>
2026-01-09 01:24:42 +0200

    build app IMAGE

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
+import { useEffect } from "react";
 import Chat from "./components/Chat";
 
 function App() {
-  return (
+  useEffect(() => {
+    const setVH = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
 
-      <Chat />
-    
-  );
+    setVH();
+    window.addEventListener("resize", setVH);
+
+    return () => window.removeEventListener("resize", setVH);
+  }, []);
+
+  return <Chat />;
 }
 
 export default App;
